4c8cd176e9a6ee05c97d190185e12c3062da1448
Regner Blok-Andersen <shadowdf@gmail.com>
2016-02-11 11:45:17 +0000

    adding en-rss-settings

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,5 @@ set -e
 gcloud config set container/cluster $1
 gcloud container clusters get-credentials $1
 
-envsubst < $2 > tmp-rc.yml
-./kubernetes/cluster/kubectl.sh delete rc $3
-./kubernetes/cluster/kubectl.sh create -f tmp-rc.yml
+./kubernetes/cluster/kubectl.sh delete rc $2
+./kubernetes/cluster/kubectl.sh create -f $3
